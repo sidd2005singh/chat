7ba62aae91add50ebf0a91c76d7477d53c4fb632
Siddharth Singh <153928349+sidd2005singh@users.noreply.github.com>
2023-12-17 15:23:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.write("This is a js code")
+const messageForm = document.getElementById('messageForm');
+const messageInput = document.getElementById('messageInput');
+const messageContainer = document.getElementById('messageContainer');
+
+messageForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const message = messageInput.value.trim();
+	if (message.length > 0) {
+		const newMessage = document.createElement('div');
+		newMessage.classList.add('message');
+		newMessage.textContent = message;
+		messageContainer.appendChild(newMessage);
+		messageInput.value = '';
+	}
+});
